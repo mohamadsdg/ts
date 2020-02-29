b517f409c6254f1280ee216fccfa6114124b54f6
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2020-02-29 13:25:38 +0330

    work with tuples

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var person = {
     name: "sdg",
     age: 26,
-    hobbies: ["sport", "game"]
+    hobbies: ["sport", "game"],
+    role: [1, "author"]
 };
+person.role.push("sdsds");
+// person.role[0] = "admin";
+person.role = [2, "admin", "super_admin"];
+console.log(person.role);
 var favoritActivites;
 favoritActivites = ["sport"];
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
--- app.ts
+++ app.ts
@@ -2,11 +2,17 @@ const person: {
   name: string;
   age: number;
   hobbies: string[];
+  role: [number, string];
 } = {
   name: "sdg",
   age: 26,
-  hobbies: ["sport", "game"]
+  hobbies: ["sport", "game"],
+  role: [1, "author"]
 };
+// person.role.push("sdsds");
+// person.role[0] = "admin";
+// person.role = [2, "admin", "super_admin"];
+console.log(person.role);
 
 let favoritActivites: string[];
 favoritActivites = ["sport"];
