058f1c8cc5e6c35255ec73879b0c8acce99b069c
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2020-03-01 15:29:03 +0330

    work with enum and any type

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "sdg",
+//   age: 26,
+//   hobbies: ["sport", "game"],
+//   role: [1, "author"]
+// };
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["AUTHOR"] = 1] = "AUTHOR";
+    Role[Role["READ_ONLY"] = 2] = "READ_ONLY";
+})(Role || (Role = {}));
 var person = {
     name: "sdg",
     age: 26,
     hobbies: ["sport", "game"],
-    role: [1, "author"]
+    role: Role[0]
 };
-person.role.push("sdsds");
+// person.role.push("sdsds");
 // person.role[0] = "admin";
-person.role = [2, "admin", "super_admin"];
-console.log(person.role);
+console.log(person.role, Role);
 var favoritActivites;
 favoritActivites = ["sport"];
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
--- app.ts
+++ app.ts
@@ -1,18 +1,28 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "sdg",
+//   age: 26,
+//   hobbies: ["sport", "game"],
+//   role: [1, "author"]
+// };
+enum Role {
+  ADMIN,
+  AUTHOR,
+  READ_ONLY
+}
+const person = {
   name: "sdg",
   age: 26,
   hobbies: ["sport", "game"],
-  role: [1, "author"]
+  role: Role[0]
 };
 // person.role.push("sdsds");
 // person.role[0] = "admin";
-// person.role = [2, "admin", "super_admin"];
-console.log(person.role);
+console.log(person.role, Role);
 
 let favoritActivites: string[];
 favoritActivites = ["sport"];
