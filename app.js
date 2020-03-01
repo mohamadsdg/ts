85b72d92c3eac7ccd683dd786348528355d155d0
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2020-03-01 17:40:06 +0330

    callback

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ function add(n1, n2) {
 function printResult(num) {
     console.log(num);
 }
-var combineValue;
-printResult(add(5, 12));
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    return cb(result);
+}
+// let combineValue: (a: number, b: number) => number;
+// printResult(add(5, 12));
 // combineValue = add;
-combineValue = add;
-console.log(combineValue(8, 8));
+// combineValue = printResult;
+// console.log(combineValue(8, 8));
+addAndHandle(6, 8, function (result) {
+    console.log(result);
+});
