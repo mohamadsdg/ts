aecf0b0ac2c336998e52ac5c420d6993f36ee2fa
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2020-03-03 19:00:44 +0330

    access modifires (public , private)

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class Department {
-  name: string;
+  public name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -7,12 +8,27 @@ class Department {
   describe(this: Department) {
     console.log(`Department ${this.name}`);
   }
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+  printEmployeeInfo() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 const accounting = new Department("Accounting");
-const accountingCopy = {
-  describe: accounting.describe,
-  name: "Accounting_copy"
-};
+
+// const accountingCopy = {
+//   describe: accounting.describe,
+//   name: "Accounting_copy"
+// };
 // console.log("accountingCopy", accountingCopy);
+// accountingCopy.describe();
+
+accounting.addEmployee("jo");
+accounting.addEmployee("morthy");
+accounting.name = "Math";
+accounting.employees[2] = "rose";
+
 accounting.describe();
-accountingCopy.describe();
+accounting.printEmployeeInfo();
