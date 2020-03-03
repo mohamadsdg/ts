8e070a5e3b7a132ecc6dade8777633d9a6938400
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2020-03-03 18:33:11 +0330

    access to blueprint obj with literal obj | know about this key word

--- src/app.ts
+++ src/app.ts
@@ -1,60 +1,18 @@
-// const userName = 'mamrez';
-// // userName = 'mohamadReza';
-// let age = 30;
+class Department {
+  name: string;
 
-// age = 29;
-
-// function add(a: number, b: number) {
-//   let result;
-//   result = a + b;
-//   return result;
-// }
-
-// if (age > 20) {
-//   let isOld = true;
-// }
-
-// console.log(isOld);
-
-// console.log(result);
-
-// const add = (a: number, b: number = 1) => a + b;
-
-// const printOutput: (a: number | string) => void = output => console.log(output);
-
-// const button = document.querySelector('button');
-
-// if (button) {
-//   button.addEventListener('click', event => console.log(event));
-// }
-
-// printOutput(add(5));
-
-const hobbies = ["Sports", "Cooking"];
-const activeHobbies = ["Hiking"];
-
-activeHobbies.push(...hobbies);
-
-const person = {
-  firstName: "Max",
-  age: 30
+  constructor(n: string) {
+    this.name = n;
+  }
+  describe(this: Department) {
+    console.log(`Department ${this.name}`);
+  }
+}
+const accounting = new Department("Accounting");
+const accountingCopy = {
+  describe: accounting.describe,
+  name: "Accounting_copy"
 };
-
-const copiedPerson = { ...person };
-
-const add = (...numbers: number[]) => {
-  return numbers.reduce((curResult, curValue) => {
-    return curResult + curValue;
-  }, 0);
-};
-
-const addedNumbers = add(5, 10, 2, 3.7);
-console.log(addedNumbers);
-
-const [hobby1, hobby2, ...remainingHobbies] = hobbies;
-
-console.log(hobbies, hobby1, hobby2);
-
-const { firstName: userName, age } = person;
-
-console.log(userName, age, person);
+// console.log("accountingCopy", accountingCopy);
+accounting.describe();
+accountingCopy.describe();
