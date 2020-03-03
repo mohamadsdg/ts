0bdef5a3581e969e78d2d95778065857202aa344
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2020-03-03 19:21:38 +0330

    work on read-only

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,23 @@
 class Department {
-  // private id: string;
+  // private readonly id: string;
   // public name: string;
-  // private employees: string[] = [];
+  private employees: string[] = [];
 
-  constructor(private id: string, public name: string) {
+  constructor(private readonly id: string, public name: string) {
     this.id = id;
     this.name = name;
   }
   describe(this: Department) {
     console.log(`Department (${this.id}) : ${this.name}`);
   }
-  // addEmployee(employee: string) {
-  //   this.employees.push(employee);
-  // }
-  // printEmployeeInfo() {
-  //   console.log(this.employees.length);
-  //   console.log(this.employees);
-  // }
+  addEmployee(employee: string) {
+    //this.id = "C2";
+    this.employees.push(employee);
+  }
+  printEmployeeInfo() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 const accounting = new Department("c1", "Accounting");
 
@@ -27,10 +28,9 @@ const accounting = new Department("c1", "Accounting");
 // };
 // accountingCopy.describe();
 
-// accounting.addEmployee("jo");
-// accounting.addEmployee("morthy");
-// accounting.name = "Math";
+accounting.addEmployee("jo");
+accounting.addEmployee("morthy");
 // accounting.employees[2] = "rose";
 
 accounting.describe();
-// accounting.printEmployeeInfo();
+accounting.printEmployeeInfo();
