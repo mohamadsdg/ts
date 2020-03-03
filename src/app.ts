9fff390d299d743adafa53651f363d76d57edc72
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2020-03-03 11:11:03 +0330

    work on nextGenJs

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,60 @@
-let userInput: unknown;
-let userName: string;
+// const userName = 'mamrez';
+// // userName = 'mohamadReza';
+// let age = 30;
 
-userInput = 5;
-userInput = "sdg";
+// age = 29;
 
-if (typeof userInput === "string") {
-  userName = userInput;
-}
+// function add(a: number, b: number) {
+//   let result;
+//   result = a + b;
+//   return result;
+// }
 
-function error(message: string): never {
-  throw { message, errorCode: 500 };
-}
-function fail() {
-  return error("Something failed");
-}
+// if (age > 20) {
+//   let isOld = true;
+// }
 
-// console.log(userName);
-fail();
+// console.log(isOld);
+
+// console.log(result);
+
+// const add = (a: number, b: number = 1) => a + b;
+
+// const printOutput: (a: number | string) => void = output => console.log(output);
+
+// const button = document.querySelector('button');
+
+// if (button) {
+//   button.addEventListener('click', event => console.log(event));
+// }
+
+// printOutput(add(5));
+
+const hobbies = ["Sports", "Cooking"];
+const activeHobbies = ["Hiking"];
+
+activeHobbies.push(...hobbies);
+
+const person = {
+  firstName: "Max",
+  age: 30
+};
+
+const copiedPerson = { ...person };
+
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
+
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+
+console.log(hobbies, hobby1, hobby2);
+
+const { firstName: userName, age } = person;
+
+console.log(userName, age, person);
