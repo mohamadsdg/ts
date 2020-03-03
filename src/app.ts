3ba896db059089d8a8322d1c2fca9c057628ae65
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2020-03-03 19:19:20 +0330

    shorthand init

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,36 @@
 class Department {
-  public name: string;
-  private employees: string[] = [];
+  // private id: string;
+  // public name: string;
+  // private employees: string[] = [];
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(private id: string, public name: string) {
+    this.id = id;
+    this.name = name;
   }
   describe(this: Department) {
-    console.log(`Department ${this.name}`);
+    console.log(`Department (${this.id}) : ${this.name}`);
   }
-  addEmployee(employee: string) {
-    this.employees.push(employee);
-  }
-  printEmployeeInfo() {
-    console.log(this.employees.length);
-    console.log(this.employees);
-  }
+  // addEmployee(employee: string) {
+  //   this.employees.push(employee);
+  // }
+  // printEmployeeInfo() {
+  //   console.log(this.employees.length);
+  //   console.log(this.employees);
+  // }
 }
-const accounting = new Department("Accounting");
+const accounting = new Department("c1", "Accounting");
 
 // const accountingCopy = {
 //   describe: accounting.describe,
-//   name: "Accounting_copy"
+//   name: "Accounting_copy",
+//   id: "C1_copy"
 // };
-// console.log("accountingCopy", accountingCopy);
 // accountingCopy.describe();
 
-accounting.addEmployee("jo");
-accounting.addEmployee("morthy");
-accounting.name = "Math";
-accounting.employees[2] = "rose";
+// accounting.addEmployee("jo");
+// accounting.addEmployee("morthy");
+// accounting.name = "Math";
+// accounting.employees[2] = "rose";
 
 accounting.describe();
-accounting.printEmployeeInfo();
+// accounting.printEmployeeInfo();
