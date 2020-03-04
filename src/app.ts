719f195d4de8b876cbd25810b14d618984018ddd
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2020-03-04 22:26:11 +0330

    interface in class

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,33 @@
-interface Person {
+interface Greetable {
   name: string;
-  age: number;
   greet(phrase: string): void;
 }
-let person1: Person;
-person1 = {
-  name: "Mohamad",
-  age: 25,
-  greet(phrase) {
-    console.log(phrase + this.name);
+
+class person implements Greetable {
+  name: string;
+  constructor(n: string) {
+    this.name = n;
   }
-};
-person1.greet("Hello ");
-console.log(person1.age);
+  greet(phrase: string) {
+    console.log(phrase + " " + this.name);
+  }
+}
+// let person_1: Greetable;
+const person_1 = new person("ALi");
+console.log(person_1);
+
+// interface Person {
+//   name: string;
+//   age: number;
+//   greet(phrase: string): void;
+// }
+// let person1: Person;
+// person1 = {
+//   name: "Mohamad",
+//   age: 25,
+//   greet(phrase) {
+//     console.log(phrase + this.name);
+//   }
+// };
+// person1.greet("Hello ");
+// console.log(person1.age);
