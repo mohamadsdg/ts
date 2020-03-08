17a41b0d91b65f285d5502a64f060f1284e8b542
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2020-03-08 16:19:54 +0330

    Type Guards

--- src/app.ts
+++ src/app.ts
@@ -31,3 +31,56 @@ type Combinable = string | number;
 type Numeric = Number | boolean;
 
 type Universal = Combinable & Numeric;
+
+// Type Guards (check which type when using Intersection)
+// Tip : 3 kind of Guard:
+//      1- check Typeof
+//      2- in (check key in object)
+//      3- instenceOf ( check class type)
+// #######################################
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknowEmployee = Admin | Employee;
+function printEmployeeInformation(emp: UnknowEmployee) {
+  console.log("name : " + emp.name);
+  if ("startDate" in emp) {
+    console.log("startDate : " + emp.startDate);
+  }
+  if ("privilages" in emp) {
+    console.log("startDate : " + emp.privilages);
+  }
+}
+// printEmployeeInformation({ name: "Mamrez", startDate: new Date() });
+
+class Car {
+  drive() {
+    console.log("Driving ....");
+  }
+}
+class Truck {
+  drive() {
+    console.log("Driving a truck ....");
+  }
+  loadCargo(amount: number) {
+    console.log("Loading Cargo " + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  vehicle instanceof Truck && vehicle.loadCargo(1000);
+}
+
+useVehicle(v1);
+useVehicle(v2);
