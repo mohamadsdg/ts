d7ac873b9e9c1e478d1f6f8e441fee3f0cb23c9d
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2020-03-15 20:14:42 +0330

    generics func

--- src/app.ts
+++ src/app.ts
@@ -19,3 +19,32 @@
 //   .catch(err => {
 //     throw err;
 //   });
+
+// Generics Functions ()
+// Tip :
+// #######################################
+
+// function merge(objectA: Object, objectB: Object) {
+//   return Object.assign(objectA, objectB);
+// }
+
+// const mergedObj = merge({ name: "sdg", hobbies: ["Sports"] }, { age: 30 });
+
+// whay dos not exist => typescript doesn't know this types we can't know this
+//                       because we are just telling it(mergedObj) you getting an object
+//                       that in their type infers that we return an object
+//                       which is correct but which doesn't carry all the information we could use here.
+// solution1 : using typecasting ( as {name:string,hobbies:string[],age:number} ) bad way :/
+// solution2 : generics help us = > We turned it into a generic function
+// tip : typescript infers that this function returns the intersection of T and U
+// tip : we tell typescript that we got two different types here but dead these types are not set in stone
+//       when we define this function but they are set dynamically when we called the function.
+//  exp : T it basically fills in an object type with an object with a name property which holds a string and a Hobbes property hich holds a array
+//  **typescript simply infers the types of the values we're passing as arguments**
+
+function merge<T, U>(objectA: T, objectB: U) {
+  return Object.assign(objectA, objectB);
+}
+const mergedObj = merge({ name: "sdg", hobbies: ["Sports"] }, { age: 30 });
+
+console.log(mergedObj);
