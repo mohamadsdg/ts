ae688f6ae9d45dfff557fcd7c077bcbf88010b2e
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2020-03-21 04:19:24 +0430

    expose decorators and multiple decorators

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ function Logger(constructor: Function) {
   console.log("Logging ...", constructor);
 }
 
-@Logger
+// @Logger
 class Person {
   name = "Mohamad";
   constructor() {
@@ -17,14 +17,16 @@ class Person {
 
 // Decorator function ()
 // Tip : Any argument of your choice and as many arguments as you want and pass a value
+//  I can add an underscore as a name which basically signals to typescript (I know I get this argument but I don't need it)
 // #######################################
 function Logger2(logString: string) {
-  return function(constructor: Function) {
-    console.log(logString, constructor);
+  //   console.log("functions first for order ");
+  return function(_: Function) {
+    console.log(logString);
   };
 }
 
-@Logger2("LoGGING  - PERSON")
+// @Logger2("LoGGING  - PERSON")
 class Person2 {
   name = "Mohamad";
   constructor() {
@@ -34,5 +36,38 @@ class Person2 {
     console.log("Dummy Log");
   }
 }
-const pers2 = new Person();
-console.log(pers2);
+// const pers2 = new Person2();
+// console.log(pers2);
+
+// Useful Decorators and multiple Decorators()
+// Tip : We add some logic Decorators which we could expose if this would be a third party library we share with our USERS
+// We could expose as part of the library and anyone who uses our library can import this decorator function and add it
+// to a class to then magically render some content all of a sudden
+// Tip2: order do these decorators execute bottom to top for Decorators
+//  for decorators functions first function and then according to order bottom to top execute decorators
+
+// #######################################
+function WithThemplate(template: string, hookId: string) {
+  //   console.log("functions second for order ");
+  return function(constructor: any) {
+    const hookElm = document.getElementById(hookId)!;
+    const person = new constructor();
+    if (hookElm) {
+      // person.dummyLog()
+      console.log(person);
+      hookElm.querySelector("h1")!.textContent = person.name;
+    }
+  };
+}
+@Logger2("LoGGING  - PERSON")
+@WithThemplate("<h1>From Themplate</h1>", "app")
+class Person3 {
+  name = "Mohamad";
+  constructor() {
+    console.log("Create Person Object");
+  }
+  dummyLog() {
+    console.log("Dummy Log");
+  }
+}
+// const pers3 = new Person3();
