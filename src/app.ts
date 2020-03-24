2b82dd0518577c3862102e1383e9657506288158
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2020-03-25 00:53:17 +0430

    themplate project list

--- src/app.ts
+++ src/app.ts
@@ -153,4 +153,45 @@ class ProjectInput {
   }
 }
 
+class ProjectList {
+  /**
+   * declare property
+   */
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+  constructor(private type: string) {
+    /**
+     * bind themplate to app
+     */
+    this.templateElement = document.getElementById(
+      "project-list"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+    /**
+     *  Register Method for Initial
+     */
+    this.attach();
+    this.renderContent();
+  }
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector(
+      "h2"
+    )!.textContent = `${this.type} PROJECTS`.toLocaleUpperCase();
+  }
+}
+
 const prjInput = new ProjectInput();
+const prjList = new ProjectList("active");
+const prjList1 = new ProjectList("finished");
